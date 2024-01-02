5933144b9b9115e4ec24bbbcfdc620530a9a2b6b
“dvphuc96” <dvphuc.sgt@gmail.com>
2024-01-03 00:45:34 +0700

    #9 handle login

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,8 +1,15 @@
 "use strict";
 
 const AuthService = require("../services/AuthService");
-const { OK, CREATED } = require("../core/successResponse");
+const { OK, CREATED, SuccessResponse } = require("../core/successResponse");
 class AuthController {
+
+  login = async (req, res, next) => {
+    new SuccessResponse({
+      metadata: await AuthService.login(req.body),
+    }).send(res);
+  }
+
   signUp = async (req, res, next) => {
     const a = new CREATED({
       message: "Sign Up Success",
--- src/services/KeyTokenService.js
+++ src/services/KeyTokenService.js
@@ -13,13 +13,21 @@ class KeyTokenService {
       return error;
     }
   };
-  static createKeyTokenBasic = async ({ userId, publicKey, privateKey }) => {
+  static createKeyTokenBasic = async ({ userId, publicKey, privateKey, refreshToken }) => {
     try {
-      const tokens = await keyTokenModel.create({
-        user: userId,
-        publicKey,
-        privateKey,
-      });
+      // level 0
+      // const tokens = await keyTokenModel.create({
+      //   user: userId,
+      //   publicKey,
+      //   privateKey,
+      // });
+      // return tokens ? tokens.publicKey : null;
+
+      // level xxx
+      const filter = { user: userId };
+      const update = { publicKey, privateKey, refreshTokenUsed: [], refreshToken };
+      const options = { upsert: true, new: true };
+      const tokens = await keyTokenModel.findOneAndUpdate(filter, update, options);
       return tokens ? tokens.publicKey : null;
     } catch (error) {
       return error;
