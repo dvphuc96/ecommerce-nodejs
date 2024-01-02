57f3c1c70fde5c478da355fb5198bf08bcf60b53
“dvphuc96” <dvphuc.sgt@gmail.com>
2024-01-02 21:51:46 +0700

    #7 handle error api

--- src/app.js
+++ src/app.js
@@ -1,25 +1,43 @@
 const compression = require("compression");
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const { default: helmet } = require("helmet");
 const morgan = require("morgan");
-const {checkOverloadConnect} = require('./helpers/check-connect');
+const { checkOverloadConnect } = require("./helpers/check-connect");
 const app = express();
 
 // init middleware
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(compression());
-app.use(express.json())
-app.use(express.urlencoded({
+app.use(express.json());
+app.use(
+  express.urlencoded({
     extended: true,
-}))
+  })
+);
 // init db
-require('./db/init.mongodb')
+require("./db/init.mongodb");
 // check overload connect
 // checkOverloadConnect()
 // init routes
-app.use('/', require("./routes"))
+app.use("/", require("./routes"));
 // handle error
 
+// middleware
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+// function handle error
+app.use((error, req, res, next) => {
+  const statusCode = error.status || 500;
+  return res.status(statusCode).json({
+    status: "error",
+    code: statusCode,
+    message: error.message || "Internal Server Error",
+  });
+});
 module.exports = app;
--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -7,117 +7,106 @@ const RoleShop = require("../constants/roles");
 const KeyTokenService = require("./KeyTokenService");
 const { createTokenPair } = require("../auth/authUtils");
 const { getInfoData } = require("../utils");
+const {
+  BadRequestError,
+  ConflictRequestError,
+  ForbiddenRequestError,
+} = require("../core/errorResponse");
 class AuthService {
   static signUp = async ({ name, email, password }) => {
-    try {
-      // check email exists??
-      const shopExists = await shopModel.findOne({ email }).lean();
-      if (shopExists) {
-        return {
-          code: "xxxx",
-          message: "Shop already registered!",
-        };
-      }
-      const passwordHash = await bcrypt.hash(password, 10);
-      const newShop = await shopModel.create({
-        name,
-        email,
-        password: passwordHash,
-        roles: [RoleShop.SHOP],
+    // check email exists??
+    const shopExists = await shopModel.findOne({ email }).lean();
+    if (shopExists) {
+      throw new ConflictRequestError("Error: Shop already registered!");
+    }
+    const passwordHash = await bcrypt.hash(password, 10);
+    const newShop = await shopModel.create({
+      name,
+      email,
+      password: passwordHash,
+      roles: [RoleShop.SHOP],
+    });
+    if (newShop) {
+      // create privateKey, publicKey use package: crypto (nâng cao dùng cho các dự án lớn (aws, cloud))
+      // sử dụng thuật toán bất đối xứng (rsa)
+      // const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
+      //   modulusLength: 4096,
+      //   publicKeyEncoding: {
+      //     type: "pkcs1", // Public key cryptoGraphy  Standards!
+      //     format: "pem",
+      //   },
+      //   privateKeyEncoding: {
+      //     type: "pkcs1", // Public key cryptoGraphy  Standards!
+      //     format: "pem",
+      //   },
+      // });
+      // const publicKeyString = await KeyTokenService.createKeyTokenAdvanced({
+      //     userId: newShop._id,
+      //     publicKey,
+      //   });
+      //   if (!publicKeyString) {
+      //     return {
+      //       code: "xxxx",
+      //       message: "Something went wrong!",
+      //       status: "error",
+      //     };
+      //   }
+
+      //   const publicKeyObject = crypto.createPublicKey(publicKeyString);
+      //   console.log("publicKeyObject::", publicKeyObject);
+
+      // create token pair
+      // const tokens = await createTokenPair(
+      //     { userId: newShop._id, email },
+      //     publicKeyObject,
+      //     privateKey
+      //   );
+      //   return {
+      //     code: 201,
+      //     metadata: {
+      //       shop: getInfoData({
+      //         fileds: ["_id", "name", "email"],
+      //         object: newShop,
+      //       }),
+      //       tokens,
+      //     },
+      //   };
+
+      // cách thông thường hay dùng (basic)
+      const privateKey = crypto.randomBytes(64).toString("hex");
+      const publicKey = crypto.randomBytes(64).toString("hex");
+      console.log(privateKey, publicKey); // save collection keystore
+
+      const keyStore = await KeyTokenService.createKeyTokenBasic({
+        userId: newShop._id,
+        publicKey,
+        privateKey,
       });
-      if (newShop) {
-        // create privateKey, publicKey use package: crypto (nâng cao dùng cho các dự án lớn (aws, cloud))
-        // sử dụng thuật toán bất đối xứng (rsa)
-        // const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
-        //   modulusLength: 4096,
-        //   publicKeyEncoding: {
-        //     type: "pkcs1", // Public key cryptoGraphy  Standards!
-        //     format: "pem",
-        //   },
-        //   privateKeyEncoding: {
-        //     type: "pkcs1", // Public key cryptoGraphy  Standards!
-        //     format: "pem",
-        //   },
-        // });
-        // const publicKeyString = await KeyTokenService.createKeyTokenAdvanced({
-        //     userId: newShop._id,
-        //     publicKey,
-        //   });
-        //   if (!publicKeyString) {
-        //     return {
-        //       code: "xxxx",
-        //       message: "Something went wrong!",
-        //       status: "error",
-        //     };
-        //   }
-
-        //   const publicKeyObject = crypto.createPublicKey(publicKeyString);
-        //   console.log("publicKeyObject::", publicKeyObject);
-
-        // create token pair
-        // const tokens = await createTokenPair(
-        //     { userId: newShop._id, email },
-        //     publicKeyObject,
-        //     privateKey
-        //   );
-        //   return {
-        //     code: 201,
-        //     metadata: {
-        //       shop: getInfoData({
-        //         fileds: ["_id", "name", "email"],
-        //         object: newShop,
-        //       }),
-        //       tokens,
-        //     },
-        //   };
-
-        // cách thông thường hay dùng (basic)
-        const privateKey = crypto.randomBytes(64).toString("hex");
-        const publicKey = crypto.randomBytes(64).toString("hex");
-        console.log(privateKey, publicKey); // save collection keystore
-
-        const keyStore = await KeyTokenService.createKeyTokenBasic({
-          userId: newShop._id,
-          publicKey,
-          privateKey,
-        });
-        if (!keyStore) {
-          return {
-            code: "xxxx",
-            message: "KeyStore error!",
-            status: "error",
-          };
-        }
-        // create token pair
-        const tokens = await createTokenPair(
-          { userId: newShop._id, email },
-          publicKey,
-          privateKey
-        );
-        console.log("Create Token Success", tokens);
-        return {
-          code: 201,
-          metadata: {
-            shop: getInfoData({
-              fileds: ["_id", "name", "email"],
-              object: newShop,
-            }),
-            tokens,
-          },
-        };
+      if (!keyStore) {
+        throw new ForbiddenRequestError("Error: KeyStore error!");
       }
+      // create token pair
+      const tokens = await createTokenPair(
+        { userId: newShop._id, email },
+        publicKey,
+        privateKey
+      );
+      console.log("Create Token Success", tokens);
       return {
-        code: 200,
-        metadata: null,
-      };
-    } catch (error) {
-      console.log(error);
-      return {
-        code: "xxx",
-        message: error.message,
-        status: "error",
+        code: 201,
+        metadata: {
+          shop: getInfoData({
+            fileds: ["_id", "name", "email"],
+            object: newShop,
+          }),
+          tokens,
+        },
       };
     }
+    return {
+      code: 200,
+      metadata: null,
+    };
   };
 }
 
