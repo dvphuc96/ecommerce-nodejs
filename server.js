435320148710612a588ad51eff73b271f4b3c0d7
“dvphuc96” <dvphuc.sgt@gmail.com>
2024-06-27 01:25:03 +0700

    test

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const app = require("./src/app");
 const express = require('express');
 const router = express.Router();
-const PORT = process.env.PORT || 3056;
-const server = app.listen(PORT, () => {
-  console.log(`wsv ecommerce start with ${PORT}`);
-});
-
+const PORT = 3056;
 router.get("/checkstatus", (req, res, next) => {
   res.status(200).json({
     status: "success",
     message: 'api ok',
   })
 });
+const server = app.listen(PORT, () => {
+  console.log(`wsv ecommerce start with ${PORT}`);
+});
+
+
 
 // process.on('SIGINT', () => {
 //     server.close(() => console.log('Exit Server Express'));
