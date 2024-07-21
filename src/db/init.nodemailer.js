ae16c741f2a995e653d2c8f31c6a0c8164631913
“dvphuc96” <dvphuc.sgt@gmail.com>
2024-07-21 22:42:06 +0700

    remove

--- src/db/init.nodemailer.js
+++ src/db/init.nodemailer.js
@@ -2,12 +2,12 @@
 
 const nodemailer = require("nodemailer");
 const transport = nodemailer.createTransport({
-  host: "email-smtp.ap-southeast-1.amazonaws.com",
+  host: "xxxx",
   port: 465,
   secure: true,
   auth: {
-    user: "AKIA4SW7WGZBCLIDGPGC",
-    pass: "BK9TllDhe+6nBtD27dpJSAM+gTTL+vJ2imrVS+OqfLYk",
+    user: "xxx",
+    pass: "xxx",
   },
 });
 
